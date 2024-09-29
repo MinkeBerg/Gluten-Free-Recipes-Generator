966a64daded4c9dd5b9bf0488ce4d5ac3ec840e9
MinkeBerg <167294739+MinkeBerg@users.noreply.github.com>
2024-09-29 20:22:39 +0200

    fixed the error 401 and improved the AIcontext

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 function displayRecipie(response) {
   console.log("recipie generated");
   new Typewriter("#recipie", {
-    strings: response.data.value,
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
@@ -15,9 +15,9 @@ function generateRecipie(event) {
   let apiKey = "39f42bod6b341c40f416ecb0tf021fa4";
   let prompt = `Generatie a glutenfree recipie about ${instructionInput.value}`;
   let context =
-    "User instructions are: You are a glutenfree recipie expert and love to write easy, simple, modern glutenfree recipies. Your mission is to generate easy, simple, delicious glutenfree recipies in basic HTML. Make sure to follow the user instructions";
+    "User instructions are: You are a glutenfree recipie expert and love to write easy, simple, modern glutenfree recipies. Your mission is to generate easy, simple, delicious glutenfree recipies in basic HTML in European cooking sizes. Start the recipie with the name of the recipie, following with the ingredients and than the instruction. Please do not put '''html at the start and no ''' at the end. The last sentense should be Enjoy your meal! in a <strong> element.  Make sure to follow the user instructions";
 
-  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=&{prompt}&context=&{context}&key=&{key}=${apiKey}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   console.log("generating recipie");
   console.log(`prompt: ${prompt}`);
