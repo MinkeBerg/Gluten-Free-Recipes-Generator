4d7082b134da604bf5bfe7c9b6b6030cc66362cb
MinkeBerg <167294739+MinkeBerg@users.noreply.github.com>
2024-09-29 11:08:11 +0200

    added basic JS form and added typewrtiting

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
+function generateRecipie(event) {
+  event.preventDefault();
 
+  let recipieElement = document.querySelector("#recipie");
+
+  new Typewriter("#recipie", {
+    strings: ["here will come the recipie"],
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let recipieFormElement = document.querySelector("#recipie-generator-form");
+
+recipieFormElement.addEventListener("submit", generateRecipie);
