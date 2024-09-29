590aaca3d705fbfc7eeddbaec1b6c217c18084c8
MinkeBerg <167294739+MinkeBerg@users.noreply.github.com>
2024-09-29 12:50:27 +0200

    added axios and she codes ai

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
-function generateRecipie(event) {
-  event.preventDefault();
-
-  let recipieElement = document.querySelector("#recipie");
-
+function displayRecipie(response) {
+  console.log("recipie generated");
   new Typewriter("#recipie", {
-    strings: ["here will come the recipie"],
+    strings: response.data.value,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generateRecipie(event) {
+  event.preventDefault();
+
+  let instructionInput = document.querySelector("#user-instruction");
+  let apiKey = "39f42bod6b341c40f416ecb0tf021fa4";
+  let prompt = `Generatie a glutenfree recipie about ${instructionInput.value}`;
+  let context =
+    "User instructions are: You are a glutenfree recipie expert and love to write easy, simple, modern glutenfree recipies. Your mission is to generate easy, simple, delicious glutenfree recipies in basic HTML. Make sure to follow the user instructions";
+
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=&{prompt}&context=&{context}&key=&{key}=${apiKey}`;
+
+  console.log("generating recipie");
+  console.log(`prompt: ${prompt}`);
+  console.log(`context: ${context}`);
+  axios.get(apiUrl).then(displayRecipie);
 }
 
 let recipieFormElement = document.querySelector("#recipie-generator-form");
